e03f4abc926c4da0f467ec509866200cffc9389f
GonzaloJCC <gonzalojimenezcarretero@gmail.com>
2026-03-22 18:12:28 +0100

    Does not work but LaTeX format is done

--- Song_Miller_Abbott/song_miller_abbott.mod
+++ Song_Miller_Abbott/song_miller_abbott.mod
@@ -1,47 +1,77 @@
 NEURON {
     POINT_PROCESS STDP
     POINTER vpre
-    RANGE g, i
-    RANGE A_plus, A_minus, tau_plus, tau_minus, g_max, E_syn
+    RANGE g, i, s, time_left_pre, time_left_post
+    RANGE A_plus, A_minus, tau_plus, tau_minus, g_max, g_min, E_syn, tau_syn, v_thresh
     NONSPECIFIC_CURRENT i
 }
 
 UNITS {
     (nA) = (nanoamp)
     (mV) = (millivolt)
-    (uS) = (microsiemens)
 }
 
-
 PARAMETER {
-    A_plus = 0.01
-    A_minus = 0.01
+    A_plus = 0.005
+    A_minus = 0.00525
     tau_plus = 20 (ms)
     tau_minus = 20 (ms)
-    g_max = 1 (uS)
+    g_max = 1.0
+    g_min = 0.0
     E_syn = 0 (mV)
+    tau_syn = 5 (ms)
+    v_thresh = -54 (mV)
 }
 
 ASSIGNED {
     v (mV)
-    vpost (mV)
     vpre (mV)
     i (nA)
-}
-
-STATE {
     g
+    s
+    time_left_pre (ms)
+    time_left_post (ms)
+    last_t (ms)
+    delta_t (ms)
 }
 
 INITIAL {
-    g = 0.05
+    s = 0
+    g = 0.005
+    time_left_pre = 0
+    time_left_post = 0
+    last_t = -1
 }
 
 BREAKPOINT {
-    vpost = v
-    i = g * (vpost - E_syn)
-}
+    if (t > last_t) {
+        s = s * exp(-dt / tau_syn)
 
-DERIVATIVE state {
-    vpost = v
+        if (time_left_pre > 0) { time_left_pre = time_left_pre - dt } else { time_left_pre = 0 }
+        if (time_left_post > 0) { time_left_post = time_left_post - dt } else { time_left_post = 0 }
+
+        if (vpre >= v_thresh && time_left_pre <= 0) {
+            time_left_pre = tau_plus
+            s = 1.0
+            if (time_left_post > 0) {
+                delta_t = time_left_pre - time_left_post
+                g = g - A_minus * exp(-delta_t / tau_minus)
+            }
+        }
+
+        if (v >= v_thresh && time_left_post <= 0) {
+            time_left_post = tau_minus
+            if (time_left_pre > 0) {
+                delta_t = time_left_pre - time_left_post
+                g = g + A_plus * exp(delta_t / tau_plus)
+            }
+        }
+
+        if (g > g_max) { g = g_max }
+        if (g < g_min) { g = g_min }
+
+        last_t = t
+    }
+
+    i = g * s * (v - E_syn)
 }
---
+++ Song_Miller_Abbott/song_miller_abbott.py
@@ -0,0 +1,101 @@
+from neuron import h
+import numpy as np
+import matplotlib.pyplot as plt
+
+# LaTeX config
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "serif",
+    "font.serif": ["Computer Modern Roman"],
+    "font.size": 11,
+    "axes.labelsize": 11,
+    "legend.fontsize": 9,
+    "xtick.labelsize": 9,
+    "ytick.labelsize": 9
+})
+
+TIME = 10000 
+SLICE = 10
+
+h.load_file('stdrun.hoc')
+h.dt = 0.005 # Equivalente a step del cpp
+
+n1 = h.Section(name='n1')
+n2 = h.Section(name='n2')
+n3 = h.Section(name='n3')
+
+n1.insert('hh')
+n2.insert('hh')
+n3.insert('hh')
+
+for n in [n1, n2, n3]:
+    n.L = 500     # Longitud en micrómetros -> sacado del cpp al definir las neuronas 1 * 7.854e-3; <- diametro
+    n.diam = 500  # Diámetro en micrómetros
+
+syn1 = h.STDP(n2(0.5))
+h.setpointer(n1(0.5)._ref_v, 'vpre', syn1)
+
+syn2 = h.STDP(n2(0.5))
+h.setpointer(n3(0.5)._ref_v, 'vpre', syn2)
+
+stim1 = h.IClamp(n1(0.5))
+stim1.delay = 0 
+stim1.dur = TIME
+stim1.amp = 600 # h1.add_synaptic_input(0.6);
+
+stim3 = h.IClamp(n3(0.5)) 
+stim3.delay = 0 
+stim3.dur = TIME
+stim3.amp = 500
+
+stim2 = h.IClamp(n2(0.5))
+stim2.delay = 0 
+stim2.dur = TIME
+stim2.amp = 500
+
+t_vec = h.Vector().record(h._ref_t)
+g1_vec = h.Vector().record(syn1._ref_g)
+g2_vec = h.Vector().record(syn2._ref_g)
+s1_vec = h.Vector().record(syn1._ref_s)
+s2_vec = h.Vector().record(syn2._ref_s)
+i1_vec = h.Vector().record(syn1._ref_i)
+i2_vec = h.Vector().record(syn2._ref_i)
+
+h.finitialize(-65)
+
+n1(0.5).v = -75
+n3(0.5).v = -85
+n2(0.5).v = -70
+
+syn1.g = 0.0052
+syn2.g = 0.005
+
+h.continuerun(TIME)
+
+
+# Gráficas
+# --- Gráficas con formato LaTeX ---
+t_np = np.array(t_vec)[SLICE:]
+g1_np = np.array(g1_vec)[SLICE:]
+g2_np = np.array(g2_vec)[SLICE:]
+
+i1_np = -np.array(i1_vec)[SLICE:] 
+i2_np = -np.array(i2_vec)[SLICE:]
+
+fig, axs = plt.subplots(2, 1, figsize=(6, 5), sharex=True)
+
+axs[0].plot(t_np, i1_np, label=r'i1', color='red')
+axs[0].plot(t_np, i2_np, label=r'i2', color='blue')
+axs[0].set_ylabel(r'Corriente ($pA$)') 
+axs[0].legend(loc='upper right')
+axs[0].grid(True, alpha=0.3)
+
+axs[1].plot(t_np, g1_np, label=r'g1', color='red')
+axs[1].plot(t_np, g2_np, label=r'g2', color='blue')
+axs[1].set_ylabel(r'Conductancia ($pS$)')
+axs[1].set_xlabel(r'Tiempo (ms)')
+axs[1].legend(loc='upper right')
+axs[1].grid(True, alpha=0.3)
+
+plt.tight_layout()
+plt.savefig('stdp_results_neuron.pdf')
